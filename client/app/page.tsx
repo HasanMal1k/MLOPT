ba74f3bd9a558a752939ffac10b650d9d963a4a5
Hasan Malik <mhasanmalik03@gmail.com>
2025-11-25 22:09:37 +0500

    changes

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -121,7 +121,7 @@ export default function Home() {
               </Link>
               
               <Link href="#how-it-works">
-                <Button size="lg" variant="outline" className="border-white/30 text-white hover:bg-white/10 text-lg px-8 py-4">
+                <Button size="lg" variant="outline" className="border-white/30 text-black/80 hover:bg-white/10 text-lg px-8 py-4">
                   Watch Demo
                 </Button>
               </Link>
